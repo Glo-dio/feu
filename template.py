79b3b43a73a5c4727dc723cb4c0a6a8aa8061f08
Glodi home <glodi@glodi-ThinkPad-P14s-Gen-3.(none)>
2023-08-14 17:01:46 +0200

    first push py = 0/6

---
+++ template.py
@@ -0,0 +1,19 @@
+import sys
+
+# fonctions utilisées
+
+
+# Partie 1 : Gestion d'erreur
+
+
+# Partie 2 : Parsing
+
+
+# Partie 3 : Résolution
+
+
+# Partie 4 : Affichage
+
+test = " "
+
+print(test)
